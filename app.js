400b8e0bd3395d1e8c1d13e8fc89f0f3a639e631
Sophie Lee <09sophie@gmail.com>
2014-04-09 21:49:00 -0700

    slide 4
    slide 4

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require ('express');
+var http = require ('http');
+var handlebars = require('express3-handlebars');
+var app = express();
+
+//configures the template engine
+app.engine ('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
+
+app.get('/', function (req, res) {
+			res.render('index');
+});
+
+app.set('port', process.env.PORT || 3000);
+
+http.createServer(app).listen(app.get('port'), function(){
+		console.log('Express server listening on port ' +
+app.get ('port'));
+});
