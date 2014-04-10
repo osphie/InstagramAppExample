4e420797fa33ae904aa38bcb7348d24aa9ff577c
Sophie Lee <09sophie@gmail.com>
2014-04-09 21:55:55 -0700

    slide 6
    slide 6

--- app.js
+++ app.js
@@ -3,15 +3,21 @@ var http = require ('http');
 var handlebars = require('express3-handlebars');
 var app = express();
 
+//load environment variable
+var dotenv = require('dotenv');
+dotenv.load();
+
 //configures the template engine
 app.engine ('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views');
 
+//routes
 app.get('/', function (req, res) {
 			res.render('index');
 });
 
+//set environment ports and start application
 app.set('port', process.env.PORT || 3000);
 
 http.createServer(app).listen(app.get('port'), function(){
