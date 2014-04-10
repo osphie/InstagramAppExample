b072eb241cab476da9f4c2121bb84b59d63a671e
Sophie Lee <09sophie@gmail.com>
2014-04-09 22:04:10 -0700

    slide 7
    slide 7

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require ('express');
 var http = require ('http');
+var path = require('path');
 var handlebars = require('express3-handlebars');
 var app = express();
 
@@ -23,11 +24,16 @@ ig.tags.info({
 app.engine ('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
 
 //routes
 app.get('/', function (req, res) {
 			res.render('index');
 });
+
+app.get('/hashtag', function (req,res) {
+	res.render('hashtag');
+})
 
 //set environment ports and start application
 app.set('port', process.env.PORT || 3000);
